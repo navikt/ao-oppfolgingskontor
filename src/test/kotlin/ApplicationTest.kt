b38c38d12fcee58266d245463dc6f0fe917c0a00
johannetronstad <johanne.tronstad@nav.no>
2025-03-27 15:16:41 +0100

    Ignorer test

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -3,11 +3,13 @@ package no.nav
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
+    @Ignore
     @Test
     fun testRoot() = testApplication {
         application {
