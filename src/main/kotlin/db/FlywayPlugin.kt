2fc5ca340f981ce3efce725f8f48e76515779eab
sigurdgroneng <sigurd.groneng@nav.no>
2025-04-08 09:41:33 +0200

    Add logging before and after flyway

--- src/main/kotlin/db/FlywayPlugin.kt
+++ src/main/kotlin/db/FlywayPlugin.kt
@@ -3,6 +3,7 @@ package no.nav.db
 import io.ktor.events.EventDefinition
 import io.ktor.server.application.*
 import org.flywaydb.core.Flyway
+import org.slf4j.LoggerFactory
 import javax.sql.DataSource
 
 val FlywayMigrationStarting: EventDefinition<Application> = EventDefinition()
@@ -14,10 +15,13 @@ class FlywayPluginConfig(
 
 val FlywayPlugin: ApplicationPlugin<FlywayPluginConfig> = createApplicationPlugin("Flyway", ::FlywayPluginConfig) {
     val dataSource = requireNotNull(pluginConfig.dataSource) { "DataSource is required for Flyway" }
+    val logger = LoggerFactory.getLogger(FlywayPlugin::class.java)
     application.monitor.raise(FlywayMigrationStarting, application)
+    logger.info("Starting Flyway migration")
     Flyway.configure()
         .dataSource(dataSource)
         .load()
         .migrate()
+    logger.info("Flyway migration finished")
     application.monitor.raise(FlywayMigrationFinished, application)
 }
