0702627e90f3afc3f4f17ad5fa1627c401709e2c
sigurdgroneng <sigurd.groneng@nav.no>
2025-04-28 13:51:02 +0200

    Utkommenter swagger / openapi

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -2,16 +2,14 @@ package no.nav
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.compression.*
-import io.ktor.server.plugins.openapi.openAPI
-import io.ktor.server.plugins.swagger.swaggerUI
-import io.ktor.server.routing.routing
 
 fun Application.configureHTTP() {
     install(Compression)
+    /*
     routing {
         openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
     }
     routing {
         swaggerUI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
-    }
+    }*/
 }
