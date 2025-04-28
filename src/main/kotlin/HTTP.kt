0cab4c3f6a0d5d3ff1ca90ebacac9772ddb93e0e
sigurdgroneng <sigurd.groneng@nav.no>
2025-04-28 14:10:44 +0200

    Take back health

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,10 +1,15 @@
 package no.nav
 
+import dab.poao.nav.no.health.healthEndpoints
 import io.ktor.server.application.*
 import io.ktor.server.plugins.compression.*
+import io.ktor.server.routing.routing
 
 fun Application.configureHTTP() {
     install(Compression)
+    routing {
+        healthEndpoints()
+    }
     /*
     routing {
         openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
