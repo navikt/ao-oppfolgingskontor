de018d70efaf0e9856ff114723c31b94c4b9555c
sigurdgroneng <sigurd.groneng@nav.no>
2025-05-16 15:18:44 +0200

    More param renaming

--- src/test/kotlin/no/nav/utils/GraphqlResponse.kt
+++ src/test/kotlin/no/nav/utils/GraphqlResponse.kt
@@ -78,12 +78,12 @@ suspend fun HttpClient.alleKontor(): HttpResponse {
 
 private fun kontorHistorikkQuery(fnr: Fnr): String {
     return graphqlPayload(fnr, """
-            { kontorHistorikk (fnrParam: \"$fnr\") { kontorId , kilde, endretAv, endretAvType, endretTidspunkt, endringsType } }
+            { kontorHistorikk (fnr: \"$fnr\") { kontorId , kilde, endretAv, endretAvType, endretTidspunkt, endringsType } }
         """.trimIndent())
 }
 fun kontorTilhorighetQuery(fnr: Fnr): String {
     return graphqlPayload(fnr, """
-             { kontorTilhorighet (fnrParam: \"$fnr\") { kontorId , kilde, registrant, registrantType, kontorNavn } }
+             { kontorTilhorighet (fnr: \"$fnr\") { kontorId , kilde, registrant, registrantType, kontorNavn } }
         """.trimIndent())
 }
 private fun alleKontorQuery(): String {
