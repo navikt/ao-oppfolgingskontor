622a89ebc844650e0230bdb8365f31219423e910
sigurdgroneng <sigurd.groneng@nav.no>
2025-05-16 15:12:02 +0200

    Rename fnrParma to fnr

--- src/main/kotlin/http/graphql/queries/KontorTilhorighetQuery.kt
+++ src/main/kotlin/http/graphql/queries/KontorTilhorighetQuery.kt
@@ -3,16 +3,13 @@ package no.nav.http.graphql.queries
 import com.expediagroup.graphql.server.operations.Query
 import graphql.schema.DataFetchingEnvironment
 import no.nav.db.Fnr
-import no.nav.db.table.ArbeidsOppfolgingKontorTable
-import no.nav.domain.KontorKilde
 import no.nav.http.graphql.schemas.KontorTilhorighetQueryDto
 import no.nav.services.KontorTilhorighetService
-import org.jetbrains.exposed.sql.*
 
 class KontorQuery(
     val kontorTilhorighetService: KontorTilhorighetService
 ) : Query {
-    suspend fun kontorTilhorighet(fnrParam: Fnr, dataFetchingEnvironment: DataFetchingEnvironment): KontorTilhorighetQueryDto? {
-        return kontorTilhorighetService.getKontorTilhorighet(fnrParam)
+    suspend fun kontorTilhorighet(fnr: Fnr, dataFetchingEnvironment: DataFetchingEnvironment): KontorTilhorighetQueryDto? {
+        return kontorTilhorighetService.getKontorTilhorighet(fnr)
     }
 }
