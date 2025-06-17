34819204073a4566854ba767cdcda66a41b7bdce
sigurdgroneng <sigurd.groneng@nav.no>
2025-06-17 07:56:22 +0200

    Fix tests

--- src/test/kotlin/no/nav/KafkaApplicationTest.kt
+++ src/test/kotlin/no/nav/KafkaApplicationTest.kt
@@ -9,6 +9,7 @@ import no.nav.db.entity.KontorHistorikkEntity
 import no.nav.db.table.KontorhistorikkTable
 import no.nav.domain.KontorId
 import no.nav.http.client.AlderFunnet
+import no.nav.http.client.FnrFunnet
 import no.nav.http.client.GTKontorFunnet
 import no.nav.http.client.arbeidssogerregisteret.ProfileringEnum
 import no.nav.kafka.consumers.EndringPaOppfolgingsBrukerConsumer
@@ -71,7 +72,7 @@ class KafkaApplicationTest {
             val consumer = OppfolgingsPeriodeConsumer(AutomatiskKontorRutingService(
                 { GTKontorFunnet(kontor) },
                 { AlderFunnet(40) },
-                { fnr },
+                { FnrFunnet(fnr) },
                 { ProfileringFunnet(ProfileringEnum.ANTATT_GODE_MULIGHETER)}
             ))
             val topology = configureTopology(listOf(topic to consumer::consume))
