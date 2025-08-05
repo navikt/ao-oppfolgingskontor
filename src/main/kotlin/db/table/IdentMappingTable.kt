e05f1e1b09adc6ffb81f1f1ae24d48ccc0ea1f0a
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-08-05 15:35:07 +0200

    Oppdater ident-mapping for å bruke auto-increment for intern ident og refaktorer lagring og henting av identer

--- src/main/kotlin/db/table/IdentMappingTable.kt
+++ src/main/kotlin/db/table/IdentMappingTable.kt
@@ -1,10 +1,6 @@
 package db.table
 
-import no.nav.db.table.ArenaKontorTable
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.timestampWithTimeZone
 import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestampWithTimeZone
 
@@ -12,7 +8,7 @@ object IdentMappingTable: IdTable<String>("ident_mapping") {
     override val id = char("ident", 20).entityId()
     val identType = char("ident_type", 11)
     val historisk = bool("historisk")
-    val internIdent = uuid("intern_ident")
+    val internIdent = long("intern_ident").autoIncrement("ident_mapping_intern_ident_seq")
     val createdAt = timestampWithTimeZone("created_at").defaultExpression(CurrentTimestampWithTimeZone)
     val updatedAt = timestampWithTimeZone("updated_at").defaultExpression(CurrentTimestampWithTimeZone)
 }
--- src/main/resources/db/migration/V16__ident_mapping_final.sql
+++ src/main/resources/db/migration/V16__ident_mapping_final.sql
@@ -1,10 +1,14 @@
+drop table if exists ident_mapping;
 create table ident_mapping (
-    ident char(20) not null primary key,
-    ident_type char(11) not null, -- Folkeregister-ident (fnr eller dnr), NPID, aktørId
+    ident varchar(20) not null primary key,
+    ident_type varchar(11) not null, -- Folkeregister-ident (fnr eller dnr), NPID, aktørId
     historisk boolean,
-    intern_ident uuid not null,
+    intern_ident serial not null,
     createdAt timestamptz default current_timestamp not null,
     updatedAt timestamptz default current_timestamp not null
 );
 
 create index idx_ident_mapping_intern_ident ON ident_mapping(intern_ident);
+
+create sequence ident_mapping_intern_ident_seq;
+
