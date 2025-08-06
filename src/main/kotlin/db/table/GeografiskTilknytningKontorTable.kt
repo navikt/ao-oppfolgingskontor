c08abd493e745cc35327e3a6a7b285025bd4580c
sigurdgroneng <sigurd.groneng@nav.no>
2025-08-06 08:53:22 +0200

    Brukt riktig id i primary key i tabeller

--- src/main/kotlin/db/table/GeografiskTilknytningKontorTable.kt
+++ src/main/kotlin/db/table/GeografiskTilknytningKontorTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.kotlin.datetime.timestampWithTimeZone
 
 object GeografiskTilknytningKontorTable: IdTable<String>("geografisktilknytningkontor") {
     override val id = varchar("fnr", 11).entityId() // VARCHAR(11) PRIMARY KEY,
-    override val primaryKey = PrimaryKey(ArenaKontorTable.id) // PRIMARY KEY (fnr),
+    override val primaryKey = PrimaryKey(id) // PRIMARY KEY (fnr),
     val kontorId = varchar("kontor_id", 4) // VARCHAR(4),
     val gt = varchar("gt", 10).nullable() // VARCHAR(4),
     val gtType = varchar("gt_type", 10).nullable() // VARCHAR(4),
