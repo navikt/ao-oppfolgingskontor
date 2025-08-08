cd6aff6d75dcae0c2c1facba1261977dcef5894c
johannetronstad <johanne.tronstad@nav.no>
2025-08-08 09:46:02 +0200

    Fix syntax

--- src/main/resources/db/migration/V16__oppfolgingsperiode_index.sql
+++ src/main/resources/db/migration/V16__oppfolgingsperiode_index.sql
@@ -1,1 +1,1 @@
-create if not exists unique index oppfolgingsperiode_id_idx on oppfolgingsperiode(oppfolgingsperiode_id);
+create unique index if not exists oppfolgingsperiode_id_idx on oppfolgingsperiode(oppfolgingsperiode_id);
