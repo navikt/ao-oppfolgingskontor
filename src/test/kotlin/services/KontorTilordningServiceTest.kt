dc7d0780355638ade638a6439840b054065e72e0
johannetronstad <johanne.tronstad@nav.no>
2025-08-26 13:35:59 +0200

    Fix test

--- src/test/kotlin/services/KontorTilordningServiceTest.kt
+++ src/test/kotlin/services/KontorTilordningServiceTest.kt
@@ -5,17 +5,14 @@ import io.kotest.matchers.shouldBe
 import no.nav.db.Fnr
 import no.nav.db.entity.ArbeidsOppfolgingKontorEntity
 import no.nav.db.entity.KontorHistorikkEntity
-import no.nav.db.table.ArbeidsOppfolgingKontorTable
 import no.nav.db.table.KontorhistorikkTable
 import no.nav.domain.OppfolgingsperiodeId
-import no.nav.domain.events.ArenaKontorEndret
 import no.nav.domain.events.OppfolgingsperiodeStartetNoeTilordning
 import no.nav.services.KontorTilordningService
 import no.nav.utils.flywayMigrationInTest
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
-import java.util.UUID
+import java.util.*
 
 
 class KontorTilordningServiceTest {
@@ -38,8 +35,8 @@ class KontorTilordningServiceTest {
             arbeidsoppfolgingskontor to historikkEntries
         }
 
-        historikkEntries.size shouldBe 2
+        historikkEntries shouldHaveSize 2
         val sisteEntry = historikkEntries.maxBy { it.id.value }
-        arbeidsoppfolgingskontor.historikkEntry shouldBe sisteEntry.id
+        arbeidsoppfolgingskontor.historikkEntry shouldBe sisteEntry.id.value
     }
 }
