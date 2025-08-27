8d178df7cdac82388b0350603eb65a62f5cd84be
sigurdgroneng <sigurd.groneng@nav.no>
2025-08-27 08:49:46 +0200

    Legg til patch for dokumentasjonsformål

---
+++ src/main/resources/db/patcher/historikk_entry.sql
@@ -0,0 +1,32 @@
+
+select count(*) from arbeidsoppfolgingskontor where historikk_entry is null;
+with arbeidsoppfolgingskontor_uten_historikk_entry as (
+    select fnr from arbeidsoppfolgingskontor
+    where historikk_entry is null
+),
+     siste_historikk_entry as (
+         select distinct on (ident) id, ident from kontorhistorikk
+             join arbeidsoppfolgingskontor_uten_historikk_entry
+         on arbeidsoppfolgingskontor_uten_historikk_entry.fnr = ident
+         where kontor_type = 'ARBEIDSOPPFOLGING'
+         order by ident, created_at desc
+     )
+update arbeidsoppfolgingskontor set historikk_entry = siste_historikk_entry.id
+    from siste_historikk_entry
+where fnr = siste_historikk_entry.ident;
+
+select count(*) from geografisktilknytningkontor where historikk_entry is null;
+with gtkontor_uten_historikk_entry as (
+    select fnr from geografisktilknytningkontor
+    where historikk_entry is null
+),
+     siste_historikk_entry as (
+         select distinct on (ident) id, ident from kontorhistorikk
+             join gtkontor_uten_historikk_entry
+         on gtkontor_uten_historikk_entry.fnr = ident
+         where kontor_type = 'GEOGRAFISK_TILKNYTNING'
+         order by ident, created_at desc
+     )
+update geografisktilknytningkontor set historikk_entry = siste_historikk_entry.id
+    from siste_historikk_entry
+where fnr = siste_historikk_entry.ident;
