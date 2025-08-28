4b1e525926f043e542809f616c65c5f0ff6576e6
Sigurd Grøneng <sigurd.groneng@nav.no>
2025-08-28 13:25:50 +0200

    Arena endretav (#50)
    * WIP: Add endretAv and endretAvType fields to ArenaKontorTable
    
    * WIP: Sett endretAv og endretAvType
    
    * Fjern endret av kolonner
    
    * Rename flyway fil
    
    ---------
    
    Co-authored-by: johannetronstad <johanne.tronstad@nav.no>
    Co-authored-by: Giil <mads.lee.giil@nav.no>

--- src/main/kotlin/db/table/ArenaKontorTable.kt
+++ src/main/kotlin/db/table/ArenaKontorTable.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.sql.kotlin.datetime.timestampWithTimeZone
 object ArenaKontorTable: IdTable<String>("arenakontor") {
     override val id = char("fnr", 11).entityId() // VARCHAR(11) PRIMARY KEY,
     override val primaryKey = PrimaryKey(id) // PRIMARY KEY (fnr),
-    val kontorId = char("kontor_id", 4) // VARCHAR(4),
+    val kontorId = char("kontor_id", 4)
     val createdAt = timestampWithTimeZone("created_at").defaultExpression(CurrentTimestampWithTimeZone) // TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     val updatedAt = timestampWithTimeZone("updated_at").defaultExpression(CurrentTimestampWithTimeZone) // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     val sistEndretDatoArena = timestampWithTimeZone("sist_endret_dato_arena")
---
+++ src/main/resources/db/migration/V24__fjern_endret_av_arena_og_gt.sql
@@ -0,0 +1,5 @@
+alter table arenakontor drop column endret_av_type;
+alter table arenakontor drop column endret_av;
+
+alter table geografisktilknytningkontor drop column endret_av_type;
+alter table geografisktilknytningkontor drop column endret_av;
