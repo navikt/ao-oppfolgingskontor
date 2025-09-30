27b304db7f209215b0cd1438a2aa1de713a79ceb
Sigurd Grøneng <sigurd.groneng@nav.no>
2025-09-30 16:29:28 +0200

    Bruk Syft til å lage SBOM (er allegedly raskere) (#59)
    * Use Cyclonedx for sbom generation
    
    * Change order of operations
    
    * Use Syft for SBOM
    
    * Set riktig output-file
    
    * Set artifact name
    
    * Use format: cyclonedx-json
    
    * Run tests and sonar in java 24
    
    * generate graphql depends on build, add sbom path
    
    * Dont upload artifact
    
    * Do upload artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,6 +138,10 @@ tasks.sonar {
     dependsOn(tasks.jacocoTestReport)
 }
 
+tasks.build {
+    dependsOn(tasks.graphqlGenerateSDL)
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "navikt_ao-oppfolgingskontor")
