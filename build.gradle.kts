c5e45532613fa14dc72c765d1381b0bb8699a9b7
Giil <mads.lee.giil@nav.no>
2025-10-09 20:33:03 +0200

    Legg tilbake blokk

--- build.gradle.kts
+++ build.gradle.kts
@@ -148,3 +148,8 @@ sonar {
         property("sonar.host.url", "https://sonarcloud.io")
     }
 }
+
+extensions.findByName("buildScan")?.withGroovyBuilder {
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+}
