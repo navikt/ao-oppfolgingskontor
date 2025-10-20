fbc689599282723a16a700a3bc08d1240470be1a
sigurdgroneng <sigurd.groneng@nav.no>
2025-10-20 14:59:20 +0200

    WIP: Kontor producer

--- src/main/kotlin/kafka/producers/KontorProducer.kt
+++ src/main/kotlin/kafka/producers/KontorProducer.kt
@@ -1,5 +1,22 @@
 package kafka.producers
 
-class KontorProducer {
+import Topics
+import no.nav.kafka.config.createKafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 
+class KontorProducer(val topics: Topics) {
+
+    val kontorTopic = topics.ut.arbeidsoppfolgingskontortilordninger
+    val producer = createKafkaProducer()
+
+    fun publiserEndringPåKontor(input: String) {
+        val key = "123"
+        val value = input
+        val record = ProducerRecord(
+            kontorTopic.name,
+            key,
+            value
+        )
+        producer.send(record)
+    }
 }
