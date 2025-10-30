f2ca138407258a64731f2d195f7f5b6a3c5f80df
Giil <mads.lee.giil@nav.no>
2025-10-30 09:41:45 +0100

    WIP: Test republisering

--- src/test/kotlin/services/KontorRepubliseringServiceTest.kt
+++ src/test/kotlin/services/KontorRepubliseringServiceTest.kt
@@ -1,7 +1,21 @@
 package services
 
-class KontorRepubliseringServiceTest {
+import no.nav.utils.TestDb
+import org.junit.jupiter.api.Test
 
+class KontorRepubliseringServiceTest {
+    private val dataSource = TestDb.postgres
+
+    @Test
+    fun `Skal kunne republisere kontor uten feil`() {
+        var republiserteKontorer = mutableListOf<KontorSomSkalRepubliseres>()
+
+        val kontorRepubliseringService = KontorRepubliseringService(
+            { republiserteKontorer.add(it) },
+            dataSource
+        )
+        kontorRepubliseringService.republiserKontorEndringer()
+    }
 
 
 }
