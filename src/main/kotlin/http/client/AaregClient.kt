001a66c7bfbd6b6642260be477729f52218e8c12
sigurdgroneng <sigurd.groneng@nav.no>
2026-01-12 13:36:51 +0100

    Start handling errors

--- src/main/kotlin/http/client/AaregClient.kt
+++ src/main/kotlin/http/client/AaregClient.kt
@@ -1,5 +1,12 @@
 package http.client
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.ApplicationEnvironment
 import no.nav.http.client.tokenexchange.ProvideToken
 
@@ -9,7 +16,18 @@ fun ApplicationEnvironment.getAaregScope(): String {
 
 class AaregClient(
     val baseUrl: String,
-    val azureTokenProvider: ProvideToken
+    val azureTokenProvider: ProvideToken,
+    val httpClient: HttpClient = HttpClient(CIO) {
+        defaultRequest {
+            url(baseUrl)
+        }
+        install(Logging) {
+            level = LogLevel.INFO
+        }
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 ) {
 
 }
--- src/main/kotlin/http/client/EregClient.kt
+++ src/main/kotlin/http/client/EregClient.kt
@@ -1,7 +1,19 @@
 package http.client
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.ApplicationEnvironment
 import no.nav.http.client.tokenexchange.ProvideToken
+import no.nav.http.client.tokenexchange.TexasTokenFailedResult
+import no.nav.http.client.tokenexchange.TexasTokenSuccessResult
 import services.OrgNummer
 
 fun ApplicationEnvironment.getEregScope(): String {
@@ -10,14 +22,38 @@ fun ApplicationEnvironment.getEregScope(): String {
 
 class EregClient(
     val baseUrl: String,
-    val azureTokenProvider: ProvideToken
+    val httpClient: HttpClient = HttpClient(CIO) {
+        defaultRequest {
+            url(baseUrl)
+        }
+        install(Logging) {
+            level = LogLevel.INFO
+        }
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 ) {
 
-    fun hentNøkkelinfoForArbeidsgiver(orgNummer: OrgNummer): EregNøkkelinfoDto {
-
+    val nøkkelInfoUrl = { orgnummer: String -> "/v2/organisasjon/$orgnummer/noekkelinfo" }
+    suspend fun hentNøkkelinfoForArbeidsgiver(orgNummer: OrgNummer): EregResult {
+        return runCatching {
+            val result = httpClient.get(nøkkelInfoUrl(orgNummer.value))
+            result.body<EregNøkkelinfoDto>()
+        }
+            .onSuccess { EregSuccess(it) }
+            .onFailure { EregFailure(it.localizedMessage) }
     }
 
 }
+
+sealed class EregResult
+class EregSuccess(
+    val data: EregNøkkelinfoDto
+): EregResult()
+class EregFailure(
+    val errorMessage: String
+): EregResult()
 
 data class EregNøkkelinfoDto(
     val adresse: Adresse
