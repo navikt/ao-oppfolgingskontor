2b4d5618a52dfb5104e01d9ae93d084bb890eb16
sigurdgroneng <sigurd.groneng@nav.no>
2026-03-18 15:49:05 +0100

    Legg til test på telling også

--- src/test/kotlin/services/KontorSammenslåingServiceTest.kt
+++ src/test/kotlin/services/KontorSammenslåingServiceTest.kt
@@ -1,6 +1,7 @@
 package services
 
 import eventsLogger.LoggSattKontorEvent
+import io.kotest.matchers.shouldBe
 import no.nav.NavAnsatt
 import no.nav.domain.KontorId
 import no.nav.domain.NavIdent
@@ -31,6 +32,21 @@ class `KontorSammenslåingServiceTest` {
             NavAnsatt(NavIdent("G112211"), UUID.randomUUID()),
             KontorSammenSlåing(listOf(kontor), KontorId("2121"))
         )
+    }
+
+    @Test
+    fun `skal telle antall personer på et kontor`() {
+        flywayMigrationInTest()
+        val loggSattKontorEvent: LoggSattKontorEvent = { a, b, c -> Unit }
+        val kontorTilordningService = KontorTilordningService(loggSattKontorEvent)
+        val service = KontorSammenslåingService(kontorTilordningService::tilordneKontor)
+        val ident = randomFnr()
+        val kontor = KontorId("8362")
+
+        val oppfolgingsperiode = gittBrukerUnderOppfolging(ident)
+        gittIdentMedKontor(ident, kontor, oppfolgingsperiode)
+
+        service.antallKontorerSomSkalEndres(listOf(kontor)) shouldBe 1
     }
 
 }
