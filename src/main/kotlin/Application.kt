c276f76d9fa681ff870acf91b170caf8d5a41fd3
Tia Firing <tia.firing@nav.no>
2026-06-02 08:45:39 +0200

    kjører jobb oftere (skal revertes)

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -246,7 +246,9 @@ fun Application.installBigQueryDailyScheduler(database: Database, bigQueryClient
         launch {
 
             while (currentCoroutineContext().isActive) {
-                val ventetid = beregnVentetid(23, 45) // Beregn hvor mange millisekunder til neste kjøring
+//                val ventetid = beregnVentetid(23, 45) // Beregn hvor mange millisekunder til neste kjøring
+                val now = ZonedDateTime.now(ZoneId.of("Europe/Oslo"))
+                val ventetid = Duration.between(now, now.plusMinutes(10)).toMillis() // For testing
 
                 val totalSeconds = ventetid / 1000
                 val hours = totalSeconds / 3600
